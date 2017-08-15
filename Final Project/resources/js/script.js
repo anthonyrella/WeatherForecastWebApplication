567bccf9c2a6f6b2b1b935e7f9af8ad5d5996a42
VladBronowicki <w.i.bronowicki@gmail.com>
2017-08-15 18:39:44 -0400

    Added Charts

--- Final Project/resources/js/script.js
+++ Final Project/resources/js/script.js
@@ -3,6 +3,7 @@ $(document).one('pagecreate', function () {
 	var search = $('#search');
 	var recent = $('#recent');
 	var searchVal;
+	var day=['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'];
 	updateWeatherCurrentLocation();
 	search.parent("div").hide();
 	var savedSearches = new Array;
@@ -45,7 +46,9 @@ $(document).one('pagecreate', function () {
 		var pos = {};
 		pos.latitude = position.coords.latitude;
 		pos.longitude = position.coords.longitude;
+		console.log("Getting Current Weather...");
 		getCurrentWeatherInfo(pos.latitude, pos.longitude);
+		console.log("Getting Current Forecast...");
 		getForecastWeather(pos.latitude, pos.longitude);
 	}
 
@@ -141,6 +144,8 @@ $(document).one('pagecreate', function () {
 					//var desc;
 					console.log(forecastInfo.dt_txt);
 				})
+				create24HourChart(data);
+				create5DayChart(data);
 			})
 			.fail(function (e) {
 				console.log(e);
@@ -149,10 +154,196 @@ $(document).one('pagecreate', function () {
 		//data pulling incomplete
 	}
 
-	function createChart() {
+	function getCityForecastWeather(cityName) {
+		$.getJSON('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=9ea3d628a6d3e398f9c7e992b0ecee16&units=metric')
+			.done(function (data) {
+				console.log(data);
+				var locationName = "Forecast location: " + data.city.name;
+				console.log(locationName);
+				$.each(data.list, function (key, forecastInfo) {
+					//var desc;
+					console.log(forecastInfo.dt_txt);
+				})
+			})
+			.fail(function (e) {
+				console.log(e);
+			})
 
+		//data pulling incomplete
 	}
 
+	function create5DayChart(forecastData) {
+		var chart_date = new Array;
+		var chart_temp = new Array;
+
+		$.each(forecastData.list, function(key, value){
+			console.log(value.dt_txt);
+			var todate = new Date(value.dt_txt);
+			
+			var tempfloat = parseFloat(value.main.temp);
+			chart_date.push(todate);
+			chart_temp.push(tempfloat);
+		});
+
+		var chart_day5d = new Array;
+		var chart_temp5d_min = new Array();
+		var chart_temp5d_max = new Array();
+		var chart_temp5d_avg = new Array();
+
+		var min= max= avg=chart_temp[0]; 
+		var curday=chart_date[0].getDay();
+
+		for(var i = 0; i < chart_temp.length; i++){
+			if(curday != chart_date[i].getDay()){
+				chart_day5d.push(day[curday]);
+				chart_temp5d_min.push(min);
+				chart_temp5d_max.push(max);
+				chart_temp5d_avg.push(avg);
+				curday=chart_date[i].getDay();
+				min= max= avg=chart_temp[i]; 
+			}
+			if(chart_temp[i] > max){
+				max=chart_temp[i];
+				avg=(min+max)/2;
+			}
+			if(chart_temp[i] < min){
+				min=chart_temp[i];
+				avg=(min+max)/2;
+			}
+			
+		}
+		chart_day5d.push(day[curday]);
+		chart_temp5d_min.push(min);
+		chart_temp5d_max.push(max);
+		chart_temp5d_avg.push(avg);
+		var cvs = document.getElementById("FiveDay");
+		var ctx = cvs.getContext('2d');
+		var chartData = {
+            type: 'line',
+            data: {
+                labels: chart_day5d,
+                datasets: [{
+                    label: "Low",
+                    fill: false,
+                    backgroundColor: "rgba(0,0,225,0.5)",
+                    borderColor: "rgba(0,0,255,1)",
+                    data: chart_temp5d_min,
+                },
+               	{
+                    label: "High",
+                    fill: false,
+                    backgroundColor: "rgba(220,0,0,0.5)",
+                    borderColor: "rgba(255,0,0,1)",
+                    data: chart_temp5d_max,
+                },
+                {
+                    label: "Avg",
+                    fill: false,
+                    backgroundColor: "rgba(0,225,0,0.5)",
+                    borderColor: "rgba(0,255,0,1)",
+                    data: chart_temp5d_avg,
+                }]
+            },
+            options: {
+                responsive: false,
+                title:{
+                    display:true,
+                    text:'Forecast'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Hours'
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Degrees C'
+                        }
+                    }]
+                }
+            }
+        };	
+		new Chart(ctx,chartData);
+	}
+	function create24HourChart(forecastData) {
+		var chart_date = new Array;
+		var chart_temp = new Array;
+		$.each(forecastData.list, function(key, value){
+			console.log(value.dt_txt);
+			var todate = new Date(value.dt_txt);
+			
+			var tempfloat = parseFloat(value.main.temp);
+			chart_date.push(todate);
+			chart_temp.push(tempfloat);
+		});
+		var chart_day24h = new Array;
+		var chart_temp24h = new Array;
+		for(var i = 0; i < 8; i++){
+			var datetext=day[chart_date[i].getDay()] + ', ' + chart_date[i].getHours() + '00';
+			chart_temp24h.push(chart_temp[i]);
+			chart_day24h.push(datetext);
+		}
+		var cvs = document.getElementById("TwentyFourHour");
+		var ctx = cvs.getContext('2d');
+		var chartData = {
+	        type: 'line',
+	        data: {
+	            labels: chart_day24h,
+	            datasets: [{
+	                label: "Low",
+	                fill: false,
+	                backgroundColor: "rgba(0,0,0,0.5)",
+	                borderColor: "rgba(0,0,0,1)",
+	                data: chart_temp24h,
+	            }]
+	        },
+	        options: {
+	            responsive: false,
+	            title:{
+	                display:true,
+	                text:'Forecast'
+	            },
+	            tooltips: {
+	                mode: 'index',
+	                intersect: false,
+	            },
+	            hover: {
+	                mode: 'nearest',
+	                intersect: true
+	            },
+	            scales: {
+	                xAxes: [{
+	                    display: true,
+	                    scaleLabel: {
+	                        display: true,
+	                        labelString: 'Hours'
+	                    }
+	                }],
+	                yAxes: [{
+	                    display: true,
+	                    scaleLabel: {
+	                        display: true,
+	                        labelString: 'Degrees C'
+	                    }
+	                }]
+	            }
+	        }
+	    };	
+		new Chart(ctx,chartData);
+	}
 	function showError() {
 
 	}
